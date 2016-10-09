af65861e43fba8ca240c28b643178f52819fbc60
Rayana Sales <rayanasales.ifpe@gmail.com>
2016-10-09 01:31:36 -0300

    Coloquei o tab view

---
+++ app/src/main/java/ifpe/surpriseme/TabsPagerAdapter.java
@@ -0,0 +1,35 @@
+package ifpe.surpriseme;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ifpe.surpriseme.fragments.CategoriesFragment;
+import ifpe.surpriseme.fragments.CurrentBackgroundFragment;
+import ifpe.surpriseme.fragments.SettingsFragment;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+        switch (index) {
+            case 0:
+                return new CurrentBackgroundFragment();
+            case 1:
+                return new CategoriesFragment();
+            case 2:
+                return new SettingsFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+
+}
---
+++ app/src/main/java/ifpe/surpriseme/fragments/SettingsFragment.java
@@ -0,0 +1,22 @@
+package ifpe.surpriseme.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ifpe.surpriseme.R;
+
+/**
+ * Created by Rayana on 10/09/2016.
+ */
+public class SettingsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return rootView;
+    }
+}
