26b450f9af4dd4782cd9d4e6f3696d0dd329cacc
nataliaamancio19 <nataliaamancio19@gmail.com>
2016-10-24 11:24:31 -0200

    Ajuste no layout para 1º Apresentação
    Ajuste no layout para 1º Apresentação

--- app/src/main/java/ifpe/surpriseme/fragments/SettingsFragment.java
+++ app/src/main/java/ifpe/surpriseme/fragments/SettingsFragment.java
@@ -1,17 +1,27 @@
 package ifpe.surpriseme.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ifpe.surpriseme.Model.Category;
+import ifpe.surpriseme.R;
 import ifpe.surpriseme.R;
 
 /**
  * Created by Rayana on 10/09/2016.
  */
+
 public class SettingsFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -19,4 +29,8 @@ public class SettingsFragment extends Fragment {
 
         return rootView;
     }
+
+
+
+
 }
