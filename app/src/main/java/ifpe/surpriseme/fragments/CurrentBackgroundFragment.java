f8c6de637dd92bbe9433f21d729e62efe185c577
Rayana Sales <rayanasales.ifpe@gmail.com>
2016-11-24 19:09:40 -0300

    Trocando wallpaper do celular

--- app/src/main/java/ifpe/surpriseme/fragments/CurrentBackgroundFragment.java
+++ app/src/main/java/ifpe/surpriseme/fragments/CurrentBackgroundFragment.java
@@ -1,14 +1,21 @@
 package ifpe.surpriseme.fragments;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -28,7 +35,7 @@ public class CurrentBackgroundFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_current_background, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_current_background, container, false);
         ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
 
         try {
@@ -39,6 +46,23 @@ public class CurrentBackgroundFragment extends Fragment {
             e.printStackTrace();
         }
         Picasso.with(getContext()).load(photoUrl).resize(950, 900).into(imageView); //largura x altura
+
+        //set system background
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+                        if(imageView != null)
+                            changeSystemBackground(imageView);
+                    }
+                },
+                15000
+        );
+
+        Toast.makeText(getActivity(), "Imagem definida como background", Toast.LENGTH_LONG).show();
+
         return rootView;
     }
 
@@ -51,5 +75,21 @@ public class CurrentBackgroundFragment extends Fragment {
         System.out.println("Tag sorteada: " + categories.get(position).getName());
 
         return categories.get(position).getName();
+    }
+
+    public void changeSystemBackground(ImageView imageSorted){
+        Bitmap bitmap = ((BitmapDrawable)imageSorted.getDrawable()).getBitmap();
+        DisplayMetrics metrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getContext());
+
+        try {
+            wallpaperManager.setBitmap(bitmap);
+            wallpaperManager.suggestDesiredDimensions(width, height);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
